10891300e873a4fafa4cf22821a73f343512c27a
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-07 17:40:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let driver = {}
+
+function updateDriverWithKeyAndValue(obj, k, v) {
+    return Object.assign({}, obj, {[k]: v})
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, k, v) {
+    obj[k] = v 
+    return obj
+}
+//using bracket notation because we want the VALUE of the parameter variable
+function deleteFromDriverByKey(obj, k) {
+    let copy_obj = {...obj}
+    delete copy_obj[k] 
+    return copy_obj
+}
+
+function destructivelyDeleteFromDriverByKey(obj, k) {
+    delete obj[k] 
+    return obj 
+}
+
